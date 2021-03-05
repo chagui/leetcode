8d2188b016fc239e706b8ad27dc052d61cb77a66
Guilhem C <guilhem.charles@gmail.com>
2021-03-05 10:50:45 +0100

    Create two-sum-ii-input-array-is-sorted.cpp

---
+++ algorithm/two-sum-ii-input-array-is-sorted.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int low = 0, high = numbers.size() - 1;
+        while (low < high) {
+            const int value = numbers[low] + numbers[high];
+            if (value == target) {
+                return {low + 1, high + 1};
+            } else if (value > target) {
+                --high;
+            } else {
+                ++low;
+            }
+        }
+        throw runtime_error("no solution");
+    }
+};
