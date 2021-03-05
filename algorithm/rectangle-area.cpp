20504e65eb9cfcc3ba7e725ce9462a6d0fdcea38
Guilhem C <guilhem.charles@gmail.com>
2021-03-05 14:04:43 +0100

    Create rectangle-area.cpp

---
+++ algorithm/rectangle-area.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+        const int areaABCD = (C - A) * (D - B);
+        const int areaEFGH = (G - E) * (H - F);
+        
+        int intersectArea = 0;
+        {
+            const int blX = max(A, E), blY = max(B, F), trX = min(C, G), trY = min(D, H);
+            if (blX < trX && blY < trY) {
+                intersectArea = (trX - blX) * (trY - blY);
+            }
+        }
+
+        return static_cast<long>(areaABCD) + areaEFGH - intersectArea; 
+    }
+};
