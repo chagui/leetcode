bf0c211531831ebb32d3451dc400864add03c4d0
Guilhem C <guilhem.charles@gmail.com>
2021-03-05 10:39:05 +0100

    Update two-sum.cpp with more solutions

--- algorithm/two-sum.cpp
+++ algorithm/two-sum.cpp
@@ -1,33 +1,57 @@
+#include <unordered_map>
+
+// default on leetcode
+using namespace std;
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        auto sorted_nums = nums;
-        sort(sorted_nums.begin(), sorted_nums.end());
-        vector<int>::size_type left = 0, right = nums.size() - 1;
-        int current = sorted_nums[left] + sorted_nums[right];
-        while (current != target) {
-            if (current > target) --right;
-            else ++left;
-            current = sorted_nums[left] + sorted_nums[right];
-        }
-        const auto left_pos = find(nums.begin(), nums.end(), sorted_nums[left]);
-        const auto right_pos = find(nums.rbegin(), nums.rend(), sorted_nums[right]);
-        return {std::distance(nums.begin(), left_pos), std::distance(nums.begin(), right_pos.base()) - 1};
+        // return twoSumBruteForce(nums, target);
+        // return twoSumHashMapTwoPasses(nums, target);
+        return twoSumHashMapOnePass(nums, target);
     }
-};
 
-
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        std::unordered_map<int, int> seen_to_index;
-        for (int idx = 0; idx < nums.size(); ++idx) {
-            const int complementary = target - nums[idx];
-            if (seen_to_index.find(complementary) != seen_to_index.end()) {
-                return {seen_to_index[complementary], idx};
+    vector<int> twoSumBruteForce(vector<int>& nums, int target) {
+        for (int valueIndex = 0; valueIndex < nums.size() - 1; ++valueIndex) {
+            const int complement  = target - nums[valueIndex];
+            for (int complementIndex = valueIndex + 1; complementIndex < nums.size(); ++complementIndex) {
+                if (nums[complementIndex] == complement ) {
+                    return {valueIndex, complementIndex};
+                }
             }
-            seen_to_index[nums[idx]] = idx;
         }
-        return {};
+        throw runtime_error("No solution");
+    }
+
+    vector<int> twoSumHashMapTwoPasses(vector<int>& nums, int target) {
+        // build value to indexes (the input may contains multiple times the same value) look-up
+        unordered_map<int, int> valueToIndex{{nums[0], 0}};
+        for (int index = 0; index < nums.size(); ++index) {
+            valueToIndex.insert_or_assign(nums[index], index);
+        }
+
+        for (int index = 1; index < nums.size() - 1; ++index) {
+            const int complement  = target - nums[index];
+            auto it = valueToIndex.find(complement );
+            if (it != valueToIndex.end() && it->second != index) {
+                return {index, it->second};
+            }
+        }
+        throw runtime_error("No solution");
+    }
+
+    vector<int> twoSumHashMapOnePass(vector<int>& nums, int target) {
+        unordered_map<int, int> valueToIndex{{nums[0], 0}};
+
+        for (int index = 1; index < nums.size(); ++index) {
+            const int value = nums[index];
+            const int complement = target - value;
+            const auto complementIt = valueToIndex.find(complement);
+            if (complementIt != valueToIndex.end() && complementIt->second != index) {
+                return {complementIt->second, index};
+            }
+            valueToIndex[value] = index;
+        }
+        throw runtime_error("No solution");
     }
 };
