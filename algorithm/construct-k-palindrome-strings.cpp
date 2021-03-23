9eb267e46b1d26e74c50d5ce0fffc96a3cb0209b
Guilhem C <guilhem.charles@gmail.com>
2021-03-23 09:22:55 +0100

    Create construct-k-palindrome-strings.cpp

---
+++ algorithm/construct-k-palindrome-strings.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool canConstruct(string s, int k) {
+        optimizedForEnglish(s, k);
+    }
+
+    // optimize solution in both space and time by assuming input will only be lower case english letters
+    bool optimizedForEnglish(string s, int k) {
+        if (s.empty() || s.length() == k) return true;
+        if (s.length() < k) return false;
+        // assumes input is always lower case english letter
+        std::bitset<26> count_is_odd;
+        for (char c : s) {
+            count_is_odd.flip(c - 'a');
+        }
+        return count_is_odd.count() <= k;
+    }
+
+    bool naive(string s, int k) {
+        if (s.empty() || s.length() == k) return true;
+        if (s.length() < k) return false;
+        std::unordered_map<char, std::size_t> count;
+        for (char c : s) {
+            ++count[c];
+        }
+        int num_of_odd = std::count_if(count.begin(), count.end(), [](const auto& i){return i.second % 2 != 0;});
+        return num_of_odd <= k;
+  }
+};
