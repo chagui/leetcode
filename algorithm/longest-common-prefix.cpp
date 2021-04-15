808b03ca8fb39b4712dd8d6779b17595555e9114
Guilhem C <guilhem.charles@gmail.com>
2021-04-15 09:42:39 +0200

    Create longest-common-prefix.cpp

---
+++ algorithm/longest-common-prefix.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.empty()) return "";
+        string prefix;
+        size_t min_length = strs.front().length();
+        for (const auto& s : strs) {
+            min_length = min(s.length(), min_length);
+        }
+        for (size_t i = 0; i < min_length; ++i) {
+            const char c = strs.front()[i];
+            for (const auto& s : strs) {
+                if(s[i] != c) return prefix;
+            }
+            prefix += c;
+        }
+        return prefix;
+    }
+};
