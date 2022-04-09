76220353e283ba589cef9c58fc2b0f0f7d296b01
G. Charles <guilhem.charles@gmail.com>
2022-04-09 18:02:22 +0200

    build search-insert-position with bazel

--- algorithm/search-insert-position.cpp
+++ algorithm/search-insert-position.cpp
@@ -1,18 +1,23 @@
-// naive
+#include <vector>
+
+using namespace std; // default on leetcode
+
+
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
+        // return naive(nums, target);
+        return binarySearch(nums, target);
+    }
+
+    int naive(vector<int>& nums, int target) {
         int index{0};
         for (;(index < nums.size()) && (target > nums[index]); ++index) {}
         return index;
     }
-};
 
-// dichotomic search
-class Solution {
-public:
-    int searchInsert(vector<int>& nums, int target) {
-        int beg {0}, end {nums.size() - 1};
+    int binarySearch(vector<int>& nums, int target) {
+        size_t beg {0}, end {nums.size() - 1};
 
         while (beg <= end) {
             int mid = beg + (end - beg) / 2;
