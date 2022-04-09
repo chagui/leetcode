dba3609e25e4ffa1e2c244bf2360a25a0df2454c
G. Charles <guilhem.charles@gmail.com>
2022-04-09 17:29:33 +0200

    build longest-substring-without-repeating-characters with bazel

--- algorithm/longest-substring-without-repeating-characters.cpp
+++ algorithm/longest-substring-without-repeating-characters.cpp
@@ -1,18 +1,23 @@
+#include <string> // default on leetcode
+#include <unordered_map>
+
+using namespace std; // default on leetcode
+
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         if (s.size() < 2) return s.size();
-        std::unordered_map<char, std::size_t> seen;
-        std:size_t longest {0}, idx {0}, start {0};
+        unordered_map<char, size_t> seen;
+        size_t longest {0}, idx {0}, start {0};
 
         for (; idx < s.size(); ++idx) {
             char c = s[idx];
             if (seen.find(c) != seen.end() && seen[c] >= start) {
-                longest = std::max(longest, idx - start);
+                longest = max(longest, idx - start);
                 start = seen[c] + 1;
             }
             seen[c] = idx;
         }
-        return std::max(longest, idx - start);
+        return max(longest, idx - start);
     }
 };
