2c8d1216a7ed32e3ae084362827e1b2818d121ad
G. Charles <guilhem.charles@gmail.com>
2022-04-15 15:48:11 +0200

    add helpers for common types

---
+++ src/helpers/list-node.h
@@ -0,0 +1,20 @@
+#ifndef LEETCODE_HELPERS_LIST_NODE_H_
+#define LEETCODE_HELPERS_LIST_NODE_H_
+
+#include <vector>
+
+namespace leetcode {
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode *buildList(const std::vector<int> &);
+bool compareList(ListNode *, ListNode *);
+void printList(ListNode *head, char sep = ' ', char eol = '\n');
+} // namespace leetcode
+
+#endif // LEETCODE_HELPERS_LIST_NODE_H_
